68d454187d98130564b07a4471e31a44d21fb9fc
unknown <amc@narola.email>
2020-06-14 00:51:28 +0530

    changes

--- application/controllers/APIs/Browse_Ctrl.php
+++ application/controllers/APIs/Browse_Ctrl.php
@@ -10,26 +10,32 @@ class Browse_Ctrl extends My_Controller {
 	{
 		parent::__construct();
 		$this->load->model('track_model','track');
+		$this->load->model('artist_model','artist');
+
 	}
 	
 	public function index()
 	{
 		$page   = $this->input->get('page', TRUE)?$this->input->get('page', TRUE):0;
 		$size   = $this->input->get('size', TRUE)?$this->input->get('size', TRUE):10;
-		$name   = $this->input->get('name', TRUE)?$this->input->get('name', TRUE):"";
+		$slug   = $this->input->get('slug', TRUE)?$this->input->get('slug', TRUE):"";
 		$content   = $this->input->get('content', TRUE)?$this->input->get('content', TRUE):10;
 		$order  = $this->input->get('order', TRUE)?$this->input->get('order', TRUE):'ASC';
 
-		$query = array('page'=>$page,'size'=>$size,'order'=>$order,'name'=>$name,'content'=>$content);
+		$query = array('page'=>$page,'size'=>$size,'order'=>$order,'where'=>array('artists.slug'=>$slug),'content'=>$content);
 		$data 		= $this->track->getBrowseTrackData($query);
-		echo '<pre>';
-		print_r($data);
-		echo '</pre>';
-		die;
+		$artist 		= $this->artist->get(array('artists.slug'=>$slug));
+		if(count($artist)){
+			$artist = $artist[0];
+		}else{
+			$artist=null;
+		}
+
 		$resp=array(
 			'message'=>"Success",
-			'data'=>$data,
-			'totalPages'=>ceil($total/$size),
+			'data'=>$artist,
+			'tracks'=>$data['aaData'],
+			'totalPages'=>ceil($data['iTotalRecords']/$size),
 			'status'=>200
 		);
 		$this->response($resp);	
